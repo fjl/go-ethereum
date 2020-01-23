b26e3396c4b2d89744c6138f5ce3243bf006b7e5
Felix Lange <fjl@twurst.com>
2020-01-24 00:18:44 +0100

    p2p: enforce maxpeers for static dial peers

--- p2p/server.go
+++ p2p/server.go
@@ -772,7 +772,7 @@ running:
 
 func (srv *Server) postHandshakeChecks(peers map[enode.ID]*Peer, inboundCount int, c *conn) error {
 	switch {
-	case !c.is(trustedConn|staticDialedConn) && len(peers) >= srv.MaxPeers:
+	case !c.is(trustedConn) && len(peers) >= srv.MaxPeers:
 		return DiscTooManyPeers
 	case !c.is(trustedConn) && c.is(inboundConn) && inboundCount >= srv.maxInboundConns():
 		return DiscTooManyPeers
