4e5dd124e65f002c08c40baa7dc17e7b73ebe0c0
Sergi Rene <sergi@datahop.network>
2022-09-26 17:02:31 +0200

    new workload script to generate registrations and lookups events in docker simulation

--- run-network-docker.sh
+++ run-network-docker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ -z "${N_NODES}" ]]; then
-  N=1000
+if [[ -z "${N}" ]]; then
+  N_NODES=4
 else
-  N=${N_NODES}
+  N_NODES=${N}
 fi
 
 #N=1000
@@ -15,6 +15,7 @@ MAX_LATENCY_MS=100
 
 PORT=32000
 RPC_PORT=22000
+
 export COMPOSE_PARALLEL_LIMIT=1000
 export COMPOSE_HTTP_TIMEOUT=300
 export DOCKER_CLIENT_TIMEOUT=300
@@ -29,16 +30,15 @@ get_bootstrap_network() {
 }
 
 get_node_network() {
-    IP3=$(($IP3+$1))
-    while test $IP3 -gt 254
+    IP3=$((IP3+$i))
+    while test $IP3 -gt 255    
     do
-      IP3=$((IP3-255))
+      IP3=$((IP3-256))
       IP2=$((IP2+1))
     done
-
-    while test $IP2 -gt 254
+    while test $IP2 -gt 255
     do
-      IP2=$((IP2-255))
+      IP2=$((IP2-256))
       IP1=$((IP1+1))
     done
     ip=$IP1"."$IP2"."$IP3
@@ -47,7 +47,8 @@ get_node_network() {
 
 start_network() {
 	docker network create -d bridge bootstrap-network --subnet=$(get_bootstrap_network).0/24 --gateway=$(get_bootstrap_network).1
-	for i in $(seq $N); do
+	for i in $(seq $N_NODES); do
+#		echo $(get_node_network $i)
 		docker network create -d bridge node$i-network --subnet=$(get_node_network $i).0/24 --gateway=$(get_node_network $i).1
 	done
 }
@@ -57,7 +58,7 @@ start_nodes() {
   logfile="$DIR/logs/node-0.log"
   
   docker run --network bootstrap-network --cap-add=NET_ADMIN --name bootstrap-node --mount type=bind,source=$PWD/discv5-test,target=/go-ethereum/$DIR devp2p sh -c "./devp2p --verbosity 5 discv5 listen --bootnodes enode://582299339f1800f3ff3238ca8772b85543b5f5d4c1fab8d0a00c274a7bcf2cccb02ea72d250ee22b912d006b5a170c15c648cc2476d738baabb8519da7a7bd70@$(get_bootstrap_network).2:0?discport=$PORT --nodekey 7fbc0a865aad6ff63baf1d16e62c07e6cc7427d1f1fc99081af758d6aa27175b --addr $(get_bootstrap_network).2:$PORT --rpc $(get_bootstrap_network).2:$RPC_PORT 2>&1 | tee $logfile " &
-  for i in $(seq $N); do
+  for i in $(seq $N_NODES); do
 
   	logfile="$DIR/logs/node-$i.log"
   	docker run --network node$i-network --cap-add=NET_ADMIN --name node$i --mount type=bind,source=$PWD/$DIR,target=/go-ethereum/$DIR devp2p sh -c "./devp2p --verbosity 5 discv5 listen --bootnodes enode://582299339f1800f3ff3238ca8772b85543b5f5d4c1fab8d0a00c274a7bcf2cccb02ea72d250ee22b912d006b5a170c15c648cc2476d738baabb8519da7a7bd70@$(get_bootstrap_network).2:0?discport=$PORT --addr $(get_node_network $i).2:$PORT --rpc $(get_node_network $i).2:$RPC_PORT 2>&1 | tee $logfile " &
@@ -69,7 +70,7 @@ start_nodes() {
 config_network() {
 
   docker exec bootstrap-node sh -c "tc qdisc add dev eth0 root netem delay $(($MIN_LATENCY_MS + $RANDOM % $MAX_LATENCY_MS))ms" &
-  for i in $(seq $N); do
+  for i in $(seq $N_NODES); do
   	docker exec node$i sh -c "tc qdisc add dev eth0 root netem delay $(($MIN_LATENCY_MS + $RANDOM % $MAX_LATENCY_MS))ms" &
         sleep 1
   done
@@ -80,7 +81,7 @@ cleanup() {
     docker stop bootstrap-node
     docker rm bootstrap-node 
     docker network rm bootstrap-network
-    for i in $(seq $N); do
+    for i in $(seq $N_NODES); do
 	docker stop node$i
 	docker rm node$i
 	 docker network rm node$i-network
@@ -94,6 +95,7 @@ sudo iptables --flush DOCKER-ISOLATION-STAGE-1
 trap cleanup EXIT
 
 start_nodes
+
 config_network
 
 wait
