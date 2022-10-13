dce48edc99325eb25197b6c2e8f59a697ceaef34
Felix Lange <fjl@twurst.com>
2022-10-13 16:00:42 +0200

    michal-run-network.sh: use config file

--- michal-run-network.sh
+++ michal-run-network.sh
@@ -2,6 +2,7 @@
 
 N_NODES=10
 DIR=discv5-test
+CONFIG_FILE=./discv5-stdconfig.json
 
 # make_keys creates all node keys.
 make_keys() {
@@ -35,7 +36,7 @@ start_nodes() {
         logfile="$DIR/logs/node-$i.log"
         echo "Starting node $i..."
         rm -f "$logfile"
-        ./devp2p --log.json --verbosity 5 discv5 listen --bootnodes "$bootnode" --nodekey "$(cat $keyfile)" --addr "127.0.0.1:$port" --rpc "127.0.0.1:$rpc" 2>&1 | tee "$logfile" &
+        ./devp2p --log.json --verbosity 5 discv5 listen --config "$CONFIG_FILE" --bootnodes "$bootnode" --nodekey "$(cat $keyfile)" --addr "127.0.0.1:$port" --rpc "127.0.0.1:$rpc" 2>&1 | tee "$logfile" &
     done
 }
 
