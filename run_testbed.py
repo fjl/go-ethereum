f6c58e75da45850ccbef4645c49a8b8dfc4ceab1
Felix Lange <fjl@twurst.com>
2022-10-24 14:30:03 +0200

    ./run_testbed.py: add option to run a chosen configuration

--- run_testbed.py
+++ run_testbed.py
@@ -17,7 +17,8 @@ def parseArguments():
     parser = argparse.ArgumentParser()
 
     # Print version
-    parser.add_argument("--docker", help="enable docker testbed",action=argparse.BooleanOptionalAction)
+    parser.add_argument("--docker", help="enable docker testbed", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--config", help="run chosen configuration", type=str)
 
     # Parse arguments
     args = parser.parse_args()
@@ -28,16 +29,51 @@ def parseArguments():
 def params_to_dir(params, type):
     result = ""
     for param in params:
-        if(features[param]['type'] == type):
+        if param in features and (features[param]['type'] == type):
             result += "_" + param + "-" + str(params[param])
     return result
 
 
-def main(docker) -> int:
-    already_run = set()
+def load_config_file(file):
+    with open(file) as f:
+        config = json.load(f)
+    assert isinstance(config, dict)
+
+    for key in features:
+        if key not in config:
+            config[key] = features[key]['default']
+
+    return config
+
+def run_it(network: Network, params: dict, is_attack=False):
+    if(is_attack):
+        out_dir = os.getcwd()+result_dir + "/attack/" + params_to_dir(params, type='attack') + "/"
+    else:
+        out_dir = os.getcwd()+result_dir + "/benign/" + params_to_dir(params, type='benign') + "/"
+        os.system('mkdir -p ' + out_dir)
+
+    run_testbed(network, out_dir, params)
+
+    workload.run_workload(network, params, out_dir)
+    print("Workload done.")
+    network.stop()
+    analysis.analyze(out_dir)
+
+
+def main(args) -> int:
+    network = NetworkLocal()
+    if args.docker:
+        network = NetworkDocker()
+    atexit.register(network.stop)
+
     params = {}
     already_run = set()
     is_attack = False
+
+    if args.config:
+        params = load_config_file(args.config)
+        run_it(network, params)
+        return
 
     for main_feature in features.keys():
         is_attack_feature = features[main_feature]['type'] == 'attack'
@@ -62,27 +98,9 @@ def main(docker) -> int:
             #pformat turns a dictionary into a string that can be added to the set
             if(pformat(params) not in already_run):
                 already_run.add(pformat(params))
-                if(is_attack):
-                    out_dir = os.getcwd()+result_dir + "/attack/" + params_to_dir(params, type='attack') + "/"
-                else:
-                    out_dir = os.getcwd()+result_dir + "/benign/" + params_to_dir(params, type='benign') + "/"
-                os.system('mkdir -p ' + out_dir)
-
-                network = NetworkLocal()
-                if docker:
-                    network = NetworkDocker()
-
-                atexit.register(network.stop)
-
-                run_testbed(network, out_dir, params)
-
-                workload.run_workload(network, params, out_dir)
-                print("Workload done.")
-                network.stop()
-                atexit.unregister(network.stop)
-                analysis.analyze(out_dir)
+                run_it(network, params, out_dir, is_attack=is_attack)
 
 
 if __name__ == '__main__':
     args = parseArguments()
-    sys.exit(main(args.docker))
+    sys.exit(main(args))
