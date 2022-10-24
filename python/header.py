bd826bdc86272eff48c307f96285c57cff36e2e1
Felix Lange <fjl@twurst.com>
2022-10-24 15:05:26 +0200

    python: remove ports in experiment settings

--- python/header.py
+++ python/header.py
@@ -5,8 +5,6 @@ features =  {'nodes': {'type': 'benign', 'default': 50,'defaultAttack': 50, 'val
              'searchBucketSize': {'type': 'benign', 'default': 3, 'defaultAttack': 3, 'vals':[3]},
              'adLifetimeSeconds': {'type': 'benign', 'default': 60, 'defaultAttack': 60, 'vals':[60]},
              'adCacheSize': {'type': 'benign', 'default': 500, 'defaultAttack': 500, 'vals':[500]},
-             'rpcBasePort': {'type': 'benign', 'default': 20200, 'defaultAttack': 20200, 'vals':[20200]},
-             'udpBasePort': {'type': 'benign', 'default': 30200, 'defaultAttack': 30200, 'vals':[30200]},
              'returnedNodes': {'type': 'benign', 'default': 30, 'defaultAttack': 1, 'vals':[30]},
 }
 
